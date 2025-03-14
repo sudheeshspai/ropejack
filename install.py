97fe0491597dd672fce85398e5d21701d16cf1a7
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2025-03-15 00:10:48 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,24 +1,32 @@
 import os
 import shutil
+import subprocess
+import sys
 
-def move_and_make_executable(file_path):
-    if not os.path.isfile(file_path):
-        print("Error: File not found!")
-        return
-    
-    bin_dirs = ["/bin", "/data/data/com.termux/files/usr/bin"]
-    file_name = os.path.basename(file_path)
-    
-    for bin_dir in bin_dirs:
-        dest_path = os.path.join(bin_dir, file_name)
-        try:
-            shutil.move(file_path, dest_path)
-            os.chmod(dest_path, 0o755)
-            print(f"File moved to {dest_path} and made executable.")
-            return
-        except PermissionError:
-            print(f"Permission denied for {dest_path}! Try running as root (sudo).")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+def check_root():
+    if os.geteuid() != 0:
+        print("This script must be run as root")
+        sys.exit(1)
 
-file_to_move = "bin/unknown"
+def main():
+    check_root()
+    termux_bin_path = "/data/data/com.termux/files/usr/bin"
+    termux_unknown_path = "/data/data/com.termux/files/usr/unknown"
+    usr_bin_path = "/usr/bin"
+    usr_unknown_path = "/usr/unknown"
+    if os.path.isdir(termux_bin_path):
+        shutil.copy("bin/unknown", termux_bin_path)
+        os.chmod(os.path.join(termux_bin_path, "unknown"), 0o755)
+        os.makedirs(termux_unknown_path, exist_ok=True)
+        shutil.copy("bin/index.html", termux_unknown_path)
+    elif os.path.isdir(usr_bin_path):
+        shutil.copy("bin/unknown", usr_bin_path)
+        os.chmod(os.path.join(usr_bin_path, "unknown"), 0o755)
+        os.makedirs(usr_unknown_path, exist_ok=True)
+        shutil.copy("bin/index.html", usr_unknown_path)
+    
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "localrun"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "vulheader"])
+
+if __name__ == "__main__":
+    main()
